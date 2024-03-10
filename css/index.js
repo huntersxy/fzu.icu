2f9db9f3cd177e55ea4c25b9cb3619d0c87856d0
huntersxy <1833505972@qq.com>
2024-03-10 17:21:19 +0800

    gx

--- css/index.js
+++ css/index.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $('.button-collapse').sideNav();
+$(document).ready(function() {
+  $('#scroll-container').on('scroll', function() {
+      if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+          $('#scroll-content').hide();
+      } else {
+          $('#scroll-content').show();
+      }
+  });
 });
