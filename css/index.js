6a83ef81dc57019589acfc7c97b9e0510a4026dd
huntersxy <1833505972@qq.com>
2024-03-11 14:46:43 +0800

    6

--- css/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#scroll-container').on('scroll', function() {
-      if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
-          $('#scroll-content').hide();
-      } else {
-          $('#scroll-content').show();
-      }
-  });
-});
